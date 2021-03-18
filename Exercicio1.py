c55e997569c4243483bf1e9ec10e89bb9f554f53
Luiz Felipe <luizfrd2003@gmail.com>
2021-03-17 22:20:26 -0300

    commit inicial

---
+++ Exercicio1.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+numero = int(input('Digite um número para ver se ele é par ou ímpar: '))
+
+if (numero%2) == 0:
+    print("Este número é Par")
+else:
+    print("Este número é Ímpar")
